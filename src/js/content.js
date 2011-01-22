c62a2197dc4ba5f1109f71c9d7b4ca913cc2a568
Chick307 <chick307@gmail.com>
2011-01-22 23:23:31 +0900

    remove unnecessary code

--- src/js/content.js
+++ src/js/content.js
@@ -5,68 +5,66 @@ var resolveLinkUrl = (function(){
 	
 	document.addEventListener('contextmenu', function(e){
 		var t = e.target;
-		if(t.tagName === 'A')
+		if(t.tagName === 'A'){
 			linkStore = {text: t.textContent, url: t.href};
+		}
 	}, false);
 	
 	return function(url){
-		if(linkStore && linkStore.url == url)
+		if(linkStore && linkStore.url == url){
 			return linkStore.text;
+		}
+		
 		alert('Failed at getting link information.', 'Google Terminal');
 		throw 'Failed at getting link information.'
 	};
 })();
 
 
-function CreateTab(){
-	chrome.extension.sendRequest({
-		method: 'CreateTab',
-		params: Array.prototype.slice.call(arguments)
-	}, function(){});
-}
+var actions = {};
 
 
-var contextMenuActions = {
-	// Gmailにシェアする
-	gmail: function(info, tab){
-		var u = 'https://mail.google.com/mail/?view=cm&fs=1&tf=1';
-		
-		if(info.selectionText){
-			u += '&su=' + encodeURIComponent(tab.title || '') +
-				'&body=' + encodeURIComponent(info.selectionText);
-		}else if(info.linkUrl){
-			u += '&su=' + encodeURIComponent(resolveLinkUrl(linkStore.url)) +
-				'&body=' + encodeURIComponent(info.linkUrl);
-		}else{
-			u += '&su=' + encodeURIComponent(tab.title || '') +
-				'&body=' + encodeURIComponent(info.pageUrl);
-		}
-		
-		CreateTab(u);
-	},
-	// Bloggerにシェアする
-	blogger: function(info, tab){
-		var u = 'http://www.blogger.com/blog-this.g';
-		
-		if(info.selectionText){
-			u += '?t=' + encodeURIComponent(info.selectionText) +
-				'&u=' + encodeURIComponent(info.pageUrl) +
-				'&n=' + encodeURIComponent(tab.title || '');
-		}else if(info.linkUrl){
-			u += '?t=&u=' + encodeURIComponent(info.linkUrl) +
-				'&n=' + encodeURIComponent(resolveLinkUrl(linkStore.url));
-		}else{
-			u += '?t=&u=' + encodeURIComponent(info.pageUrl) +
-				'&n=' + encodeURIComponent(tab.title || '');
-		}
-		
-		CreateTab(u);
+// Gmailにシェアする
+actions.gmail = function(info, tab){
+	var u = 'https://mail.google.com/mail/?view=cm&fs=1&tf=1';
+	
+	if(info.selectionText){
+		u += '&su=' + encodeURIComponent(tab.title || '') +
+			'&body=' + encodeURIComponent(info.selectionText);
+	}else if(info.linkUrl){
+		u += '&su=' + encodeURIComponent(resolveLinkUrl(info.linkUrl)) +
+			'&body=' + encodeURIComponent(info.linkUrl);
+	}else{
+		u += '&su=' + encodeURIComponent(tab.title || '') +
+			'&body=' + encodeURIComponent(info.pageUrl);
 	}
+	
+	window.open(u);
+};
+
+// Bloggerにシェアする
+actions.blogger = function(info, tab){
+	var u = 'http://www.blogger.com/blog-this.g';
+	
+	if(info.selectionText){
+		u += '?t=' + encodeURIComponent(info.selectionText) +
+			'&u=' + encodeURIComponent(info.pageUrl) +
+			'&n=' + encodeURIComponent(tab.title || '');
+	}else if(info.linkUrl){
+		u += '?t=&u=' + encodeURIComponent(info.linkUrl) +
+			'&n=' + encodeURIComponent(resolveLinkUrl(info.linkUrl));
+	}else{
+		u += '?t=&u=' + encodeURIComponent(info.pageUrl) +
+			'&n=' + encodeURIComponent(tab.title || '');
+	}
+	
+	window.open(u);
 };
 
 
 // リクエストを受け付ける
 chrome.extension.onRequest.addListener(function(req, sender, sendResponse){
-	if(req.action in contextMenuActions)
-		contextMenuActions[req.action](req.info, req.tab, sendResponse);
+	if(req.action in actions){
+		actions[req.action](req.info, req.tab, sendResponse);
+	}
 });
--- src/js/service.js
+++ src/js/service.js
@@ -1,406 +1,371 @@
 
 function Service(args){
-	this.id = args.id;
-	this.name = args.name;
-	this.url = args.url;
-	this.icon = args.icon || 'icons/google.png';
+	Object.defineProperties(this, {
+		id: {value: args.id},
+		name: {value: args.name},
+		url: {value: args.url},
+		icon: {value: args.icon || 'icons/google.png'},
+		menus: {value: args.menus || []},
+		urlContainsScheme: {value: /^[a-z]+:/.test(args.url)},
+		onEnabled: {value: []},
+		onDisabled: {value: []},
+		isEnabled: {value: false, writable: true},
+		menuIds: {value: [], writable: true}
+	});
 	
-	this.setSecure(true);
-	this.setEnabled(true);
+	if(args.id === 'gmail'){
+		Gmail.call(this);
+	}else if(args.id === 'reader'){
+		GoogleReader.call(this);
+	}
 	
-	this.onInitialize = args.onInitialize;
-	
-	this.scheduleManager = null;
-	this.contextMenuManager = null;
-	this.badgeManager = null;
+	if(pref.get(args.id + '-enabled', true)){
+		this.enable();
+	}
 }
 
-Service.prototype = {
-	initialize: function(man){
-		if(this.enabled && this.onInitialize)
-			this.onInitialize(man);
-	},
-	open: function(){
-		var self = this;
+Object.defineProperties(Service.prototype, {
+	enable: {value: function(){
+		if(this.isEnabled){
+			return;
+		}
+		
+		this.isEnabled = true;
+		pref.set(this.id + '-enabled', true);
+		
+		// コンテキストメニューを作成
+		this.menuIds = this.menus.map(function(menu){
+			return chrome.contextMenus.create({
+				type: menu.type || 'normal',
+				title: menu.title,
+				contexts: [menu.context],
+				onclick: function(info, tab){
+					chrome.tabs.sendRequest(tab.id, {
+						action: menu.action,
+						info: info,
+						tab: tab
+					});
+				}
+			});
+		});
+		
+		this.onEnabled.forEach(function(onEnabled){
+			onEnabled.call(this);
+		}, this);
+	}},
+	disable: {value: function(){
+		if(!this.isEnabled){
+			return;
+		}
+		
+		this.isEnabled = false;
+		pref.set(this.id + '-enabled', false);
+		
+		// コンテキストメニューを削除
+		this.menuIds.forEach(function(menuId){
+			chrome.contextMenus.remove(menuId);
+		});
+		this.menuIds = [];
+		
+		this.onDisabled.forEach(function(onDisabled){
+			onDisabled.call(this);
+		}, this);
+	}},
+	open: {value: function(){
+		var url, secure = pref.get('secure', true);
+		if(this.urlContainsScheme){
+			url = this.url;
+		}else{
+			url = (secure? 'https://': 'http://') + this.url;
+		}
+		
 		chrome.tabs.getAllInWindow(null, function(tabs){
 			for(var i = 0, tab; tab = tabs[i]; i++){
-				if(tab.url && self.checkUrl(tab.url)){
+				if(tab.url && tab.url.indexOf(url) === 0){
 					chrome.tabs.update(tab.id, {selected: true});
 					return;
 				}
 			}
-			
-			chrome.tabs.create({url: self.fullUrl, selected: true});
-		});
-	},
-	checkUrl: function(url){
-		if(url.indexOf(this.fullUrl) != 0)
-			return false;
-		
-		return (url.length == this.fullUrl.length) ||
-			(url[this.fullUrl.length] == '?') ||
-			(url[this.fullUrl.length] == '#') ||
-			(url[this.fullUrl.length] == '/');
-	},
-	getInfo: function(){
-		return {
-			id: this.id,
-			name: this.name,
-			url: this.url,
-			icon: this.icon,
-			secure: this.secure,
-			enabled: this.enabled
-		};
-	},
-	getSecure: function(){
-		return this.secure;
-	},
-	setSecure: function(value){
-		var url = this.url;
-		this.secure = value? true: false;
-		if(/^https?:\/\//.test(url))
-			this.fullUrl = this.url;
-		else
-			this.fullUrl = (value? 'https://': 'http://') + url;
-	},
-	getEnabled: function(){
-		return this.enabled;
-	},
-	setEnabled: function(value){
-		return this.enabled = (value? true: false);
-	}
-};
+			chrome.tabs.create({url: url, selected: true});
+		}.bind(this));
+	}}
+});
 
 
-function ServiceManager(services){
-	this.initialized = false;
-	this.services = services;
+function Gmail(){
+	var polling = null, pollInterval = 1000 * 60 * 5;
 	
-	this.scheduleManager = new ScheduleManager();
-	this.contextMenuManager = new ContextMenuManager();
-	this.badgeManager = new BadgeManager();
+	// フィードURL
+	var feed = (pref.get('secure', true)? "https://": "http://") +
+		"mail.google.com/mail/feed/atom";
+	
+	// XMLネームスペース
+	var ns = function(prefix){
+		if(prefix == 'gmail'){
+			return 'http://purl.org/atom/ns#';
+		}
+	};
+	
+	var checkUnreadCount = function(){
+		// TODO
+		new HttpRequest({
+			url: feed,
+			onSuccess: function(args){
+				var fullCountNode = args.xml.evaluate(
+					"/gmail:feed/gmail:fullcount", args.xml, ns,
+					XPathResult.ANY_TYPE, null
+				).iterateNext();
+				
+				if(fullCountNode){
+					if(fullCountNode.textContent == '0'){
+						badge.gmail = null;
+					}else{
+						badge.gmail = fullCountNode.textContent;
+					}
+				}else{
+					console.error('Gmail XML Error', args);
+				}
+			},
+			onError: function(e){
+				badge.gmail = null;
+				console.error('Gmail Error :', e);
+			}
+		});
+	}.bind(this);
+	
+	// サービスのページが開かれたら未読チェック
+	var onTabUpdated = function(tabId, changeInfo, tab){
+		var url, secure = pref.get('secure', true);
+		if(this.urlContainsScheme){
+			url = this.url;
+		}else{
+			url = (secure? 'https://': 'http://') + this.url;
+		}
+		
+		if(tab.url && tab.url.indexOf(url) === 0){
+			checkUnreadCount();
+		}
+	}.bind(this);
+	
+	this.onEnabled.push(function(){
+		checkUnreadCount();
+		
+		// 未読チェックを開始
+		polling = setInterval(function(){
+			checkUnreadCount();
+		}, pollInterval);
+		
+		// タブを監視する
+		if(!chrome.tabs.onUpdated.hasListener(onTabUpdated)){
+			chrome.tabs.onUpdated.addListener(onTabUpdated);
+		}
+	});
+	
+	this.onDisabled.push(function(){
+		badge.gmail = null;
+		
+		// 未読チェックを終了
+		if(polling){
+			clearInterval(polling);
+			polling = null;
+		}
+		
+		// タブの監視を外す
+		if(chrome.tabs.onUpdated.hasListener(onTabUpdated)){
+			chrome.tabs.onUpdated.removeListener(onTabUpdated)
+		}
+	});
 }
 
-ServiceManager.prototype = {
-	initialize: function(isEnabled, isSecure){
-		this.scheduleManager.initialize();
-		this.contextMenuManager.initialize();
-		this.badgeManager.initialize();
-		
-		this.allService = {};
-		this.allServiceInfo = [];
-		
-		var self = this;
-		this.services.forEach(function(service){
-			service.setSecure(isSecure(service.id));
-			service.setEnabled(isEnabled(service.id));
-			
-			service.scheduleManager = self.scheduleManager;
-			service.contextMenuManager = self.contextMenuManager;
-			service.badgeManager = self.badgeManager;
-			
-			service.initialize(self);
-			
-			self.allService[service.id] = service;
-			self.allServiceInfo.push(service.getInfo());
+
+function GoogleReader(){
+	var polling = null, pollInterval = 1000 * 60 * 5;
+	
+	// JSON URL
+	var json = (pref.get('secure')? "https://": "http://") +
+		"www.google.com/reader/api/0/unread-count?output=json";
+	
+	var checkUnreadCount = function(){
+		// TODO
+		new HttpRequest({
+			url: json,
+			onSuccess: function(args){
+				if(args.status != 200){
+					console.error('Status Code Error "' + args.status + '"');
+					return;
+				}
+				
+				var linkList = args.json.unreadcounts, value = null;
+				
+				for(var i in linkList){
+					if(linkList[i].id.indexOf("reading-list") >= 0){
+						value = linkList[i].count.toString();
+						break;
+					}
+				}
+				
+				badge.reader = value;
+			},
+			onError: function(e){
+				badge.reader = null;
+				console.error('Google Reader Error :', e);
+			}
 		});
+	}.bind(this);
+	
+	// サービスのページが開かれたら未読チェック
+	var onTabUpdated = function(tabId, changeInfo, tab){
+		var url, secure = pref.get('secure', true);
+		if(this.urlContainsScheme){
+			url = this.url;
+		}else{
+			url = (secure? 'https://': 'http://') + this.url;
+		}
 		
-		this.contextMenuManager.createAllMenus();
+		if(tab.url && tab.url.indexOf(url) === 0){
+			checkUnreadCount();
+		}
+	}.bind(this);
+	
+	this.onEnabled.push(function(){
+		checkUnreadCount();
 		
-		this.initialized = true;
-	},
-	getAllServiceInfo: function(){
-		if(this.initialized)
-			return this.allServiceInfo;
-		throw 'ServiceManager is not initialized.';
-	},
-	openService: function(id){
-		if(id in this.allService)
-			this.allService[id].open();
-		else
-			throw 'Service which id is ' + id + ' do not exists.';
-	}
-};
+		// 未読チェックを開始
+		polling = setInterval(function(){
+			checkUnreadCount();
+		}, pollInterval);
+		
+		// タブを監視する
+		if(!chrome.tabs.onUpdated.hasListener(onTabUpdated)){
+			chrome.tabs.onUpdated.addListener(onTabUpdated);
+		}
+	});
+	
+	this.onDisabled.push(function(){
+		badge.reader = null;
+		
+		// 未読チェックを終了
+		if(polling){
+			clearInterval(polling);
+			polling = null;
+		}
+		
+		// タブの監視を外す
+		if(chrome.tabs.onUpdated.hasListener(onTabUpdated)){
+			chrome.tabs.onUpdated.removeListener(onTabUpdated)
+		}
+	});
+}
 
 
-var ServiceList = [
-	new Service({
-		id: 'gmail',
-		name: 'Gmail',
-		url: 'mail.google.com/mail',
-		icon: 'icons/gmail.png',
-		onInitialize: function(man){
-			var self = this;
-			
-			// Badge
-			man.badgeManager.setColor(self.id, [208, 0, 24, 255]);
-			var setBadge = function(value){
-				man.badgeManager.setValue(self.id, (value == 0)? null: value);
-			};
-			
-			// Context Menus
-			man.contextMenuManager.createMenu({
-				title: 'Mail This Page',
-				context: 'page',
-				action: 'gmail'
-			});
-			man.contextMenuManager.createMenu({
-				title: 'Mail This Link',
-				context: 'link',
-				action: 'gmail'
-			});
-			man.contextMenuManager.createMenu({
-				title: 'Mail This Text',
-				context: 'selection',
-				action: 'gmail'
-			});
-			
-			// Check Unread Count
-			var feed = (self.getSecure()? "https://": "http://") +
-				"mail.google.com/mail/feed/atom";
-				//"mail.google.com/" + (domain || "mail") + "/feed/atom";
-			var ns = function(prefix){
-				if(prefix == 'gmail')
-					return 'http://purl.org/atom/ns#';
-			};
-			var requestFailureCount = 0;
-			var checkUnreadCount = function(){
-				new HttpRequest({
-					url: feed,
-					onSuccess: function(args){
-						var fullCountSet = args.xml.evaluate(
-							"/gmail:feed/gmail:fullcount", args.xml, ns,
-							XPathResult.ANY_TYPE, null
-						);
-						var fullCountNode = fullCountSet.iterateNext();
-						
-						if(fullCountNode){
-							setBadge(fullCountNode.textContent);
-							requestFailureCount = 0;
-						}else{
-							throw 'XML Error';
-						}
-					},
-					onError: function(e){
-						setBadge(null);
-						requestFailureCount += 1;
-						console.error('Gmail Error :', e);
-					}
-				});
-			};
-			
-			// Schedule to Check Unread Count
-			var pollInterval = {min: 1000 * 60, max: 1000 * 60 * 30};
-			var getInterval = function(){
-				var exponent = Math.pow(2, requestFailureCount + 1);
-				return Math.round(Math.min(
-					Math.random() * pollInterval.min * exponent,
-					pollInterval.max
-				));
-			};
-			var checkUnreadCountTask = function(){
-				if(!self.getEnabled())
-					return;
-				checkUnreadCount();
-				man.scheduleManager.set(
-					self.id, getInterval(), checkUnreadCountTask);
-			};
-			
-			// Listen to Tabs Updated
-			if(!self.tabUpdatedListener){
-				self.tabUpdatedListener = function(tabId, changeInfo){
-					if(self.getEnabled()){
-						self.onTabUpdated(tabId, changeInfo);
-					}else{
-						chrome.tabs.onUpdated.removeListener(
-							self.tabUpdatedListener);
-					}
-				};
-			}
-			this.onTabUpdated = function(tabId, changeInfo){
-				if(changeInfo.url && self.checkUrl(changeInfo.url))
-					checkUnreadCount();
-			};
-			if(!chrome.tabs.onUpdated.hasListener(self.tabUpdatedListener))
-				chrome.tabs.onUpdated.addListener(self.tabUpdatedListener);
-			
-			checkUnreadCountTask();
-		}
-	}),
-	new Service({
-		id: 'calendar',
-		name: 'Google Calendar',
-		url: 'www.google.com/calendar',
-		icon: 'icons/calendar.png'
-	}),
-	new Service({
-		id: 'reader',
-		name: 'Google Reader',
-		url: 'www.google.com/reader',
-		icon: 'icons/reader.png',
-		onInitialize: function(man){
-			var self = this;
-			
-			// Badge
-			man.badgeManager.setColor(self.id, [0, 24, 208, 255]);
-			var setBadge = function(value){
-				man.badgeManager.setValue(self.id, (value == 0)? null: value);
-			};
-			
-			// Check Unread Count
-			var feed = (self.secure? "https://": "http://") +
-				"www.google.com/reader/api/0/unread-count?output=json";
-			var checkUnreadCount = function(){
-				new HttpRequest({
-					url: feed,
-					onSuccess: function(args){
-						if(args.status != 200)
-							throw 'Status Code Error "' + args.status + '"';
-						var linkList = args.json.unreadcounts;
-						for(var i in linkList){
-							if(linkList[i].id.indexOf("reading-list") >= 0){
-								setBadge(linkList[i].count.toString());
-								return;
-							}
-						}
-						setBadge(null);
-					},
-					onError: function(e){
-						setBadge(null);
-						console.error('Google Reader Error :', e);
-					}
-				});
-			};
-			
-			// Schedule to Check Unread Count
-			var checkUnreadCountTask = function(){
-				if(!self.getEnabled())
-					return;
-				checkUnreadCount();
-				man.scheduleManager.set(
-					self.id, 1000 * 60 * 3, checkUnreadCountTask);
-			};
-			
-			// Listen to Tabs Updated
-			if(!self.tabUpdatedListener){
-				self.tabUpdatedListener = function(tabId, changeInfo){
-					if(self.getEnabled()){
-						self.onTabUpdated(tabId, changeInfo);
-					}else{
-						chrome.tabs.onUpdated.removeListener(
-							self.tabUpdatedListener);
-					}
-				};
-			}
-			this.onTabUpdated = function(tabId, changeInfo){
-				if(changeInfo.url && self.checkUrl(changeInfo.url))
-					checkUnreadCount();
-			};
-			if(!chrome.tabs.onUpdated.hasListener(self.tabUpdatedListener))
-				chrome.tabs.onUpdated.addListener(self.tabUpdatedListener);
-			
-			checkUnreadCountTask();
-		}
-	}),
-	new Service({
-		id: 'contacts',
-		name: 'Contacts',
-		url: 'www.google.com/contacts',
-		icon: 'icons/contacts.png'
-	}),
-	new Service({
-		id: 'tasks',
-		name: 'Tasks',
-		url: 'mail.google.com/tasks/canvas',
-		icon: 'icons/tasks.png'
-	}),
-	new Service({
-		id: 'docs',
-		name: 'Google Docs',
-		url: 'docs.google.com',
-		icon: 'icons/docs.png'
-	}),
-	new Service({
-		id: 'sites',
-		name: 'Google Sites',
-		url: 'sites.google.com',
-		icon: 'icons/sites.png'
-	}),
-	new Service({
-		id: 'analytics',
-		name: 'Analytics',
-		url: 'www.google.com/analytics/settings/home',
-		icon: 'icons/analytics.png'
-	}),
-	new Service({
-		id: 'tools',
-		name: 'Webmaster Tools',
-		url: 'www.google.com/webmasters/tools/home',
-		icon: 'icons/webmaster.png'
-	}),
-	new Service({
-		id: 'feed',
-		name: 'FeedBurner',
-		url: 'feedburner.google.com',
-		icon: 'icons/feedburner.png'
-	}),
-	new Service({
-		id: 'blog',
-		name: 'Blogger',
-		url: 'www.blogger.com/home',
-		icon: 'icons/blogger.png',
-		onInitialize: function(man){
-			man.contextMenuManager.createMenu({
-				title: 'Blog This Page',
-				context: 'page',
-				action: 'blogger'
-			});
-			man.contextMenuManager.createMenu({
-				title: 'Blog This Link',
-				context: 'link',
-				action: 'blogger'
-			});
-			man.contextMenuManager.createMenu({
-				title: 'Blog This Text',
-				context: 'selection',
-				action: 'blogger'
-			});
-		}
-	}),
-	new Service({
-		id: 'adsense',
-		name: 'Adsense',
-		url: 'www.google.com/adsense',
-	}),
-	new Service({
-		id: 'appengine',
-		name: 'AppEngine',
-		url: 'appengine.google.com',
-		icon: 'icons/appengine.png'
-	}),
-	new Service({
-		id: 'picasa',
-		name: 'Picasa',
-		url: 'picasaweb.google.com/home',
-		icon: 'icons/picasa.png'
-	}),
-	new Service({
-		id: 'youtube',
-		name: 'YouTube',
-		url: 'www.youtube.com',
-		icon: 'icons/youtube.png'
-	}),
-	new Service({
-		id: 'dashboard',
-		name: 'Dashboard',
-		url: 'https://www.google.com/dashboard/',
-		icon: 'icons/google.png'
-	}),
-	new Service({
-		id: 'accounts',
-		name: 'Accounts',
-		url: 'https://www.google.com/accounts/',
-		icon: 'icons/google.png'
-	})
-];
-
+var services = [{
+	id: 'gmail',
+	name: 'Gmail',
+	url: 'mail.google.com/mail',
+	icon: 'icons/gmail.png',
+	menus: [{
+		title: 'Mail This Page',
+		context: 'page',
+		action: 'gmail'
+	}, {
+		title: 'Mail This Link',
+		context: 'link',
+		action: 'gmail'
+	}, {
+		title: 'Mail This Text',
+		context: 'selection',
+		action: 'gmail'
+	}]
+}, {
+	id: 'calendar',
+	name: 'Google Calendar',
+	url: 'www.google.com/calendar',
+	icon: 'icons/calendar.png'
+}, {
+	id: 'reader',
+	name: 'Google Reader',
+	url: 'www.google.com/reader',
+	icon: 'icons/reader.png'
+}, {
+	id: 'contacts',
+	name: 'Contacts',
+	url: 'www.google.com/contacts',
+	icon: 'icons/contacts.png'
+}, {
+	id: 'tasks',
+	name: 'Tasks',
+	url: 'mail.google.com/tasks/canvas',
+	icon: 'icons/tasks.png'
+}, {
+	id: 'docs',
+	name: 'Google Docs',
+	url: 'docs.google.com',
+	icon: 'icons/docs.png'
+}, {
+	id: 'sites',
+	name: 'Google Sites',
+	url: 'sites.google.com',
+	icon: 'icons/sites.png'
+}, {
+	id: 'analytics',
+	name: 'Analytics',
+	url: 'www.google.com/analytics/settings/home',
+	icon: 'icons/analytics.png'
+}, {
+	id: 'tools',
+	name: 'Webmaster Tools',
+	url: 'www.google.com/webmasters/tools/home',
+	icon: 'icons/webmaster.png'
+}, {
+	id: 'feed',
+	name: 'FeedBurner',
+	url: 'feedburner.google.com',
+	icon: 'icons/feedburner.png'
+}, {
+	id: 'blog',
+	name: 'Blogger',
+	url: 'www.blogger.com/home',
+	icon: 'icons/blogger.png',
+	menus: [{
+		title: 'Blog This Page',
+		context: 'page',
+		action: 'blogger'
+	}, {
+		title: 'Blog This Link',
+		context: 'link',
+		action: 'blogger'
+	}, {
+		title: 'Blog This Text',
+		context: 'selection',
+		action: 'blogger'
+	}]
+}, {
+	id: 'adsense',
+	name: 'Adsense',
+	url: 'www.google.com/adsense',
+}, {
+	id: 'appengine',
+	name: 'AppEngine',
+	url: 'appengine.google.com',
+	icon: 'icons/appengine.png'
+}, {
+	id: 'picasa',
+	name: 'Picasa',
+	url: 'picasaweb.google.com/home',
+	icon: 'icons/picasa.png'
+}, {
+	id: 'youtube',
+	name: 'YouTube',
+	url: 'www.youtube.com',
+	icon: 'icons/youtube.png'
+}, {
+	id: 'dashboard',
+	name: 'Dashboard',
+	url: 'https://www.google.com/dashboard/'
+}, {
+	id: 'accounts',
+	name: 'Accounts',
+	url: 'https://www.google.com/accounts/'
+}].map(function(args){
+	return new Service(args);
+});
